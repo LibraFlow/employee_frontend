780f4e848f7c901a1cde58914c2431d01c7af676
i459452 <459452@student.fontys.nl>
2025-06-23 21:31:41 +0200

    Increasing timeouts

--- employee_frontend/cypress.config.js
+++ employee_frontend/cypress.config.js
@@ -8,20 +8,7 @@ module.exports = defineConfig({
     video: false,
     screenshotOnRunFailure: true,
     defaultCommandTimeout: 15000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
-    pageLoadTimeout: 30000,
     viewportWidth: 1280,
     viewportHeight: 720,
-    retries: {
-      runMode: 2,
-      openMode: 0
-    },
-    experimentalRunAllSpecs: true,
-    experimentalModifyObstructiveThirdPartyCode: true,
-    chromeWebSecurity: false,
-    env: {
-      CYPRESS_CI: true
-    }
   },
 }) 
